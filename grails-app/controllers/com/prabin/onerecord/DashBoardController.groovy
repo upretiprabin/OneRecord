dc9eb3986bd3737304cd24c55dd7caa903cb43d7
prabin <pupreti@deerwalk.com>
2017-10-23 09:38:48 +0545

    created test branch

--- grails-app/controllers/com/prabin/onerecord/DashBoardController.groovy
+++ grails-app/controllers/com/prabin/onerecord/DashBoardController.groovy
@@ -1,20 +1,40 @@
 package com.prabin.onerecord
 
 import grails.converters.JSON
+import grails.core.GrailsApplication
 import org.grails.web.json.JSONObject
 import org.grails.web.json.parser.JSONParser
+import org.joda.time.DateTime
 
 class DashBoardController {
 
     def chartService;
+    GrailsApplication grailsApplication;
 
     def index = {
 
         // read from json file
-        JSONParser parser = new JSONParser(new FileReader("onerecord.json"));
-        JSONObject a = (JSONObject) parser.parse();
+        String jsonFileName = grailsApplication.config.jsonFileName;
+        JSONParser parser = new JSONParser(new FileReader(new File(jsonFileName).getAbsolutePath()));
+        JSONObject jsonObj = (JSONObject) parser.parse();
+        Map<String,Object> chartSeries = new HashMap<String,Object>();
+        chartSeries.put("name",jsonObj.memberFullName)
+        ArrayList<Double> tempList = new ArrayList<Double>();
+        jsonObj.monthlyData.forEach{key,value->
+            tempList.add((Double)value.getAt("medicalPaidAmount"))
+        }
+        chartSeries.put("data",tempList)
+        String chartScript = chartService.getDashBoardChartScript(chartSeries as JSON);
 
-        String chartScript = chartService.getDashBoardChartScript();
+
+        DateTime today = new DateTime();
+
+        DateTime tomorrow = today.plusDays(1);
+
+        System.out.println(today.toString("yyyy-MMM-dd"));
+
+        System.out.println(tomorrow.toString("yyyy-MMMM-dd"));
+
         [chartOptions: chartScript]
     }
 }
